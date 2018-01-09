5c421bc9379365058c9512e271d4c5dd7ece294e
brendon-boldt <brendon_boldt@yahoo.com>
2018-01-09 01:03:28 -0500

    Move header and footer to js files

---
+++ footer.js
@@ -0,0 +1,87 @@
+let footerHtml = `
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<!--
+			<section class="3u 6u(narrower) 12u$(mobilep)">
+			-->
+			<section class="6u 6u(narrower) 12u$(mobilep)">
+				<h3>Links</h3>
+				<ul class="links">
+					<li><a href="https://github.com/brendon-boldt/brendon-boldt.github.io/tree/master/files/brendon_boldt_cv.pdf">Resume</a></li>
+					<li><a href="https://github.com/brendon-boldt/">GitHub</a></li>
+					<li><a href="https://www.linkedin.com/in/brendonjboldt/">LinkedIn</a></li>
+					<li><a href="https://scholar.google.com/citations?user=QEXlK3AAAAAJ&hl=en&oi=ao">Google Scholar</a></li>
+					<li><a href="https://www.researchgate.net/profile/Brendon_Boldt">Research Gate</a></li>
+				</ul>
+			</section>
+			<section class="6u 6u$(narrower) 12u$(mobilep)">
+				<h3>Email Haiku</h3>
+				Given name, surname<br/>
+				and a dot in between at<br/>
+				the com of gmail
+
+			</section>
+			<!--
+			<section class="3u 6u$(narrower) 12u$(mobilep)">
+				<h3>More Links to Stuff</h3>
+				<ul class="links">
+					<li><a href="#">Duis neque nisi dapibus</a></li>
+					<li><a href="#">Sed et dapibus quis</a></li>
+					<li><a href="#">Rutrum accumsan sed</a></li>
+					<li><a href="#">Mattis et sed accumsan</a></li>
+					<li><a href="#">Duis neque nisi sed</a></li>
+					<li><a href="#">Sed et dapibus quis</a></li>
+					<li><a href="#">Rutrum amet varius</a></li>
+				</ul>
+			</section>
+			<section class="6u 12u(narrower)">
+				<h3>Get In Touch</h3>
+				<form>
+					<div class="row 50%">
+						<div class="6u 12u(mobilep)">
+							<input type="text" name="name" id="name" placeholder="Name" />
+						</div>
+						<div class="6u 12u(mobilep)">
+							<input type="email" name="email" id="email" placeholder="Email" />
+						</div>
+					</div>
+					<div class="row 50%">
+						<div class="12u">
+							<textarea name="message" id="message" placeholder="Message" rows="5"></textarea>
+						</div>
+					</div>
+					<div class="row 50%">
+						<div class="12u">
+							<ul class="actions">
+								<li><input type="submit" class="button alt" value="Send Message" /></li>
+							</ul>
+						</div>
+					</div>
+				</form>
+			</section>
+			-->
+		</div>
+	</div>
+
+	<!-- Icons -->
+		<ul class="icons">
+			<!--
+			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+			-->
+			<li><a href="https://github.com/brendon-boldt/" class="icon fa-github"><span class="label">GitHub</span></a></li>
+			<li><a href="https://www.linkedin.com/in/brendonjboldt/" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+		</ul>
+
+	<!-- Copyright -->
+		<div class="copyright">
+			<ul class="menu">
+				<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+			</ul>
+		</div>
+
+</div>
+`;
+document.write(footerHtml);
---
+++ header.js
@@ -0,0 +1,47 @@
+let current = document.currentScript.getAttribute('data-current');
+let currentObj = {};
+currentObj[current] = 'current';
+let headerHtml = `
+<div id="header">
+
+	<!-- Logo -->
+	<h1><a href="index.html" id="logo">Brendon Boldt<em>: Computer Scientist, Philosopher, Machine Teacher</em></a></h1>
+
+	<!-- Nav -->
+		<nav id="nav">
+			<ul>
+				<li class="${currentObj['home']}"><a href="index.html">Home</a></li>
+				<li class="${currentObj['research']}"><a href="research.html">Research</a></li>
+				<li class="${currentObj['philosophy']}"><a href="#">Philosophy</a></li>
+				<li class="${currentObj['contact']}"><a href="#footer">Contact</a></li>
+				<!--
+				<li>
+					<a href="#">Dropdown</a>
+					<ul>
+						<li><a href="#">Lorem dolor</a></li>
+						<li><a href="#">Magna phasellus</a></li>
+						<li><a href="#">Etiam sed tempus</a></li>
+						<li>
+							<a href="#">Submenu</a>
+							<ul>
+								<li><a href="#">Lorem dolor</a></li>
+								<li><a href="#">Phasellus magna</a></li>
+								<li><a href="#">Magna phasellus</a></li>
+								<li><a href="#">Etiam nisl</a></li>
+								<li><a href="#">Veroeros feugiat</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Veroeros feugiat</a></li>
+					</ul>
+				</li>
+				<li><a href="left-sidebar.html">Left Sidebar</a></li>
+				<li><a href="right-sidebar.html">Right Sidebar</a></li>
+				<li><a href="two-sidebar.html">Two Sidebar</a></li>
+				<li><a href="no-sidebar.html">No Sidebar</a></li>
+				-->
+			</ul>
+		</nav>
+
+</div>
+`;
+document.write(headerHtml);
