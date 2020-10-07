5c98c3de78472899ceb063ff458147ee9d23f082
Brendon Boldt <brendon.boldt@gmail.com>
2020-10-07 11:02:49 -0400

    Remove Google Analytics

--- footer.js
+++ footer.js
@@ -85,14 +85,5 @@ let footerHtml = `
 		</div>
 
 </div>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113184953-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-113184953-1');
-</script>
 `;
 document.write(footerHtml);
