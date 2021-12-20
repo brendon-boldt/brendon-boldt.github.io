e0a922abb5d1d6cda80be5b464c336d18d7c8d79
Brendon Boldt <brendon.mb@protonmail.com>
2021-12-19 20:21:37 -0500

    Add most carols

--- caroling/generate.py
+++ caroling/generate.py
@@ -19,6 +19,121 @@ metadata = [
         "title": "Joy to the World",
         "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/joy_to_the_world-1.htm",
     },
+    {
+        "file": "fathers_love",
+        "title": "Of the Father's Love Begotten",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/of_the_fathers_love_begotten-1.htm",
+    },
+    {
+        "file": "angel_gabriel",
+        "title": "The Angel Gabriel from Heaven Came",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/gabriels_message.htm",
+    },
+    # {
+    #     "file": "praise_ye_the_lord_of_hosts",
+    #     "title": "Praise Ye the Lord of Hosts",
+    #     "source": "",
+    # },
+    {
+        "file": "wenceslas",
+        "title": "Good King Wenceslas",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/good_king_wenceslas.htm",
+    },
+    {
+        "file": "ding_dong",
+        "title": "Ding Dong Merrily on High",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/ding_dong_merrily_on_high.htm",
+    },
+    {
+        "file": "puer_natus",
+        "title": "Puer Natus in Bethlehem",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/NonEnglish/puer_natus_in_bethlehem-dreves.htm",
+    },
+    {
+        "file": "rose_eer",
+        "title": "Lo, How a Rose E'er Blooming",
+        "source": "",
+    },
+    {
+        "file": "home_alone",
+        "title": "Somewhere in My Memory (Home Alone)",
+        "source": "https://genius.com/John-williams-home-alone-main-title-somewhere-in-my-memory-lyrics",
+    },
+    {
+        "file": "dominick",
+        "title": "Dominick the Donkey",
+        "source": "https://www.lyrics.com/lyric/22100837/Dominick+the+Donkey",
+    },
+    {
+        "file": "lord_of_hosts",
+        "title": "Praise Ye the Lord",
+        "source": "https://hymnary.org/text/o_praise_the_blessed_lord_of_hosts",
+    },
+    {
+        "file": "ye_faithful",
+        "title": "O Come, All Ye Faithful",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/o_come_all_ye_faithful2.htm",
+    },
+    {
+        "file": "adeste",
+        "title": "Adeste Fidelis",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/NonEnglish/adeste_fideles.htm",
+    },
+    {
+        "file": "midnight",
+        "title": "It Came Upon the Midnight Clear",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/it_came_upon_the_midnight_clear.htm",
+    },
+    {
+        "file": "angels_we_have_heard_on_high",
+        "title": "Angels We Have Heard on High",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/angels_we_have_heard_on_high_1.htm",
+    },
+    {
+        "file": "wassail",
+        "title": "Wassail! Wassail! All Over the Town",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/wassail_wassail_all_over_1.htm",
+    },
+    {
+        "file": "o_holy_night",
+        "title": "O Holy Night",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/o_holy_night.htm",
+    },
+    {
+        "file": "o_little_town_of_bethlehem",
+        "title": "O Little Town of Bethlehem",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/o_little_town_of_bethlehem.htm",
+    },
+    {
+        "file": "go_tell_it_on_the_mountain",
+        "title": "Go Tell It on the Mountain",
+        "source": "https://www.azlyrics.com/lyrics/forkingcountry/gotellitonthemountain.html",
+    },
+    {
+        "file": "we_three_kings",
+        "title": "We Three Kings",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/we_three_kings_of_orient_are.htm",
+    },
+    {
+        "file": "the_first_noel",
+        "title": "The First Noel",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/first_noel.htm",
+    },
+    {
+        "file": "hark_the_herald",
+        "title": "Hark! The Hearld Angels Sing",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/hark_the_herald_angels_sing.htm",
+    },
+    {
+        "file": "little_drummer_boy",
+        "title": "Little Drummer Boy",
+        "source": "http://www.songlyrics.com/traditional-christmas/the-little-drummer-boy-lyrics/",
+    },
+    {
+        "file": "i_saw_three_ships",
+        "title": "I Saw Three Ships",
+        "source": "http://www.hymnsandcarolsofchristmas.com/Hymns_and_Carols/i_saw_three_ships.htm",
+    },
     # {
     #     "file": "",
     #     "title": "",
@@ -29,6 +144,7 @@ metadata = [
 # <script type="text/javascript">setTimeout(() => window.location.reload(true), 3000);</script>
 HEADER = """
 <html>
+<meta charset="UTF-8">
 <head>
 </head>
 <body>
@@ -50,12 +166,16 @@ def make_index():
 
 
 def make_carol(datum):
+    if datum["file"] == "":
+        return
     yield f"<h3 id='{datum['file']}'>{datum['title']}</h3>"
+    yield "<p>"
     with open(f"carols/{datum['file']}") as fo:
         for line in fo.readlines():
             yield f"{line} <br/>"
+    yield "</p>"
     yield "<br><i><a href='#top'>Back to top</a></i>"
-    yield "<br><br><i><a href='{datum['source']}'>Source website</a></i>"
+    yield f"<br><br><i><a target='blank' href='{datum['source']}'>Source website</a></i><br/><br/>"
 
 
 def write_gen(fo, gen):
